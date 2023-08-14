094a977ad666f792f3865fca992d2460612ff052
carloseduribeiro <carloseribeiro96@gmail.com>
2023-08-14 17:14:43 -0300

    removing select from exchange client in server

--- server/api/handler.go
+++ server/api/handler.go
@@ -6,7 +6,6 @@ import (
 	"github.com/carloseduribeiro/Client-Server-API/server/client/exchange"
 	"log"
 	"net/http"
-	"time"
 )
 
 const (
@@ -29,29 +28,23 @@ func (e *ExchangeHandler) GetExchange(w http.ResponseWriter, r *http.Request) {
 	log.Println("iniciarndo request:", r.Method, r.RequestURI)
 	defer log.Println("request finalizada")
 	ctx := r.Context()
-	select {
-	case <-time.After(200 * time.Millisecond):
-		exchangeResult, err := e.getExchange(ctx, coins)
-		if err != nil {
-			if exchangeResult != nil && len(exchangeResult) == 0 {
-				writeResponseErr(w, http.StatusNotFound, err)
-				return
-			}
-			if errors.Is(err, context.DeadlineExceeded) {
-				writeResponseErr(w, http.StatusRequestTimeout, err)
-				return
-			}
-			writeResponseErr(w, http.StatusInternalServerError, err)
+	exchangeResult, err := e.getExchange(ctx, coins)
+	if err != nil {
+		if exchangeResult != nil && len(exchangeResult) == 0 {
+			writeResponseErr(w, http.StatusNotFound, err)
 			return
 		}
-		err = e.repository.Save(ctx, &exchangeResult[0])
-		if err != nil {
-			writeResponseErr(w, http.StatusInternalServerError, err)
+		if errors.Is(err, context.DeadlineExceeded) {
+			writeResponseErr(w, http.StatusRequestTimeout, err)
 			return
 		}
-		_ = writeJSON(w, http.StatusOK, exchangeResult)
-	case <-ctx.Done():
-		log.Println("request finalizada pelo usuário")
+		writeResponseErr(w, http.StatusInternalServerError, err)
 		return
 	}
+	err = e.repository.Save(ctx, &exchangeResult[0])
+	if err != nil {
+		writeResponseErr(w, http.StatusInternalServerError, err)
+		return
+	}
+	_ = writeJSON(w, http.StatusOK, exchangeResult)
 }
